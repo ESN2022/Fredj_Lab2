3ed155b1ccd2f74d97d3af7b86494864c5cd9329
FirasFredj404 <84923813+FirasFredj404@users.noreply.github.com>
2023-01-13 02:57:19 +0100

    3 digit counter + BCD convertor on Soft

--- lab2.vhd
+++ lab2.vhd
@@ -6,7 +6,10 @@ entity Lab2 is
 	port (                    
 		clk: in  std_logic;
 		reset : in std_logic;
-		display : out  STD_LOGIC_VECTOR (7 downto 0)	
+		display1 : out  STD_LOGIC_VECTOR (7 downto 0);
+		display2 : out  STD_LOGIC_VECTOR (7 downto 0);
+		display3 : out  STD_LOGIC_VECTOR (7 downto 0)
+
 	);
 end entity Lab2;
 
@@ -17,39 +20,32 @@ architecture rtl of Lab2 is
 
    component Lab2_sys is
         port (
-            clk_clk                              : in  std_logic                    := 'X'; -- clk
-            reset_reset_n                        : in  std_logic                    := 'X'; -- reset_n
-            afficheur_external_connection_export : out std_logic_vector(3 downto 0)         -- export
+            afficheur_external_connection_export : out std_logic_vector(11 downto 0);        -- export
+            clk_clk                              : in  std_logic                     := 'X'; -- clk
+            reset_reset_n                        : in  std_logic                     := 'X'
         );
-    end component Lab2_sys;
+    end component;
 	 
-	 signal readbin: std_logic_vector(3 downto 0);
+	 component decode is
+	port (                    
+		input : in STD_LOGIC_VECTOR (3 downto 0);
+		output: out STD_LOGIC_VECTOR (7 downto 0)
+	);
+	end component;
+	
+	 
+	 signal readbin: std_logic_vector(11 downto 0);
 	 
 begin
      u0 : component Lab2_sys
         port map (
+		  afficheur_external_connection_export   => readbin,
 		  clk_clk       => clk,
-		  reset_reset_n => reset,
-        afficheur_external_connection_export   => readbin  
-            
-               
+		  reset_reset_n => reset               
         );
 		  
-decodage : process(readbin)
-    begin
-        case readbin is
-            when "0000" => display <= "00000011";
-            when "0001" => display <= "10011111";
-            when "0010" => display <= "00100101";
-            when "0011" => display <= "00001101";
-            when "0100" => display <= "10011011";
-            when "0101" => display <= "01001011";
-            when "0110" => display <= "01000001";
-            when "0111" => display <= "00011111";
-            when "1000" => display <= "00000001";
-            when "1001" => display <= "00001001";
-            when others => display <= "01100000";
-        end case;
-    end process;
+		 u1: component decode port map (readbin(3 downto 0), display1);
+		 u2: component decode port map (readbin(7 downto 4), display2);
+		 u3: component decode port map (readbin(11 downto 8), display3);
 	
 end architecture;
--- software/app/main.c
+++ software/app/main.c
@@ -4,16 +4,19 @@
 #include "altera_avalon_pio_regs.h"
 #include "sys/alt_irq.h"
 
+
 int main()
 { 
   alt_putstr("Hello from Nios II!\n");
-  int inc=0x00;
-  /* Event loop never exits. */
+  int  inc=0;
+  int aff;
   while (1){
-	  IOWR_ALTERA_AVALON_PIO_DATA(AFFICHEUR_BASE,inc);
+	  // in this command we will transform a 3 digit int to a 12 bits BCD number
+      aff = (inc % 10) | (((inc / 10) % 10) << 4) | ((inc /100) << 8);// same as aff=inc%10+((inc/10)%10)*16+(inc/100)*256; but better
+	  IOWR_ALTERA_AVALON_PIO_DATA(AFFICHEUR_BASE,aff);
 	  inc++;
-	  if (inc==0x09) inc=0x00;
-	  usleep(1000000);
+	  if (inc==1000) {inc=0;} // to restart once it's over 999
+	  usleep(90000);
   }
 
   return 0;
